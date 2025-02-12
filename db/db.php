469b26769a086e6bb0b5bdb0c91d75d6737c10e7
mokawa <ro-man700@mail.ru>
2025-02-12 21:42:42 +0500

    Улучшение

--- db/db.php
+++ db/db.php
@@ -1,6 +1,11 @@
 <?php
 
-$pdo = new PDO('mysql:host=mysql-8.2;dbname=db_application;charset=utf8', 'root', '', [
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-]);
+try{
+    $pdo = new PDO('mysql:host=mysql-8.2;dbname=db_application;charset=utf8', 'root', '', [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+}
+catch(Exception $e){
+    echo 'Нет подключения к базе';
+}
 ?>
